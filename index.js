7b3217f38ea6b13c5fa3ffa2b639ce13092c1495
khai96_ <hvksmr1996@gmail.com>
2016-05-22 16:32:25 +0700

    rename 'rm' --> '__'

--- index.js
+++ index.js
@@ -64,8 +64,8 @@
 		addPromise((resolve) => __rm(entry, (...errinf) => resolve(errinf), onaction))
 			.onfinish((errinf) => onfinish(...errinf));
 
-	var rm = module.exports = (entry, onfinish = _throwif, onaction = _donothing) => _rm(resolvePath(entry), onfinish, onaction);
+	var __ = module.exports = (entry, onfinish = _throwif, onaction = _donothing) => _rm(resolvePath(entry), onfinish, onaction);
 
-	rm.withoutPromise = __rm;
+	__.withoutPromise = __rm;
 
 })(module);
